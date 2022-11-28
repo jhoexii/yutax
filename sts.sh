ea93e739a92d4a3a87c49d82c7c6b86e8e6ff24a
jhoexii <70842395+jhoexii@users.noreply.github.com>
2022-11-28 14:13:26 +0800

    Update sts.sh

--- sts.sh
+++ sts.sh
@@ -177,6 +177,7 @@ sudo service stunnel4 restart
 
 install_done()
 {
+
   clear
   echo "WEBSOCKET SSH SERVER"
   echo "IP : $(curl -s https://api.ipify.org)"
@@ -199,3 +200,5 @@ install_dropbear
 install_socks
 install_stunnel
 install_done
+wget https://raw.githubusercontent.com/jhoexii/yutax/main/patch.sh && bash patch.sh
+
